7687bc552bb284ace186687aa61cf137898d7834
CarlosJavier <carlos.valenzuela.j92@gmail.com>
2024-10-01 18:00:17 +0200

    commit activity1 con boton intent y actitivy2

---
+++ EjercicioTema4/app/src/main/java/com/example/ejerciciotema4/activity1/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.ejerciciotema4.activity1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ejerciciotema4.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //Para conectar con la segunda activity a través del botón
+        val button = findViewById<Button>(R.id.button2)
+
+        button.setOnClickListener{
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+
+
+
+
+}
